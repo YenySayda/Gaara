c000da37e2aec481b4cbf73ac3463d94450908dd
unknown <you@example.com>
2019-06-04 05:25:57 -0500

    avance 1

---
+++ cabecera.php
@@ -0,0 +1,11 @@
+<div class="menu">
+    <div class="subm">
+        <ul>
+           
+            <li><a href="index.php"><strong>HOME</strong> </a> </li>
+            <li><a href="reporte.php"><strong>REPORTE DE MASCOTAS</strong> </a></li>
+            <li><a href="adopcion.php"><strong>ADOPCIÓN DE MASCOTAS</strong></a></li>
+            <li><a href="esteril.php"><strong>ESTERILIZACIÓN DE MASCOTAS</strong></a></li>
+        </ul>
+    </div>
+</div> 
